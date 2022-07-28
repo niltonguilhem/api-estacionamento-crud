c24bc79484f83673f644402db803d37c6b4df457
Nilton Guilhem Junior <njguilhem@gmail.com>
2022-07-28 02:20:26 -0300

    comentários para refinamento

--- src/main/java/com/ngtechnology/estacionamento/controller/EstacionamentoController.java
+++ src/main/java/com/ngtechnology/estacionamento/controller/EstacionamentoController.java
@@ -15,20 +15,26 @@ public class EstacionamentoController {
     @Autowired
     private VagaService service;
 
+
+    /** modificar a nomenclatura do método, talvez para getAllVagas **/
     @GetMapping()
     public Iterable<Vagas> get() {
         return service.findEstacionamento();}
 
+    /** modificar a nomenclatura do método, talvez para getIdVagas **/
     @GetMapping("/{id}")
     public Optional<Vagas> get(@PathVariable("id") Long idvaga) {
         return service.getVagaById(idvaga);
     }
 
+    /** modificar a nomenclatura do método, talvez para postVagas, lembrando que as nomenclaturas save, delete, find, são mais recorrentes
+     * nas services que interagem com o banco de dados **/
     @PostMapping
     public ResponseEntity<Vagas> save(@RequestBody Vagas vagas){
         return new ResponseEntity<>(service.save(vagas),HttpStatus.CREATED);
     }
 
+    /** modificar a nomenclatura do método, talvez para putVagas **/
     @PutMapping("/{id}")
     public ResponseEntity<Vagas> put (@PathVariable ("id") Long id, @RequestBody Vagas vagas){
         Vagas v = service.update(vagas,id);
--- src/main/java/com/ngtechnology/estacionamento/controller/IndexController.java
+++ src/main/java/com/ngtechnology/estacionamento/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class IndexController {
-
+ /** não precisa dessa controller **/
     @GetMapping()
     public  String get() { return  "API de controle de vagas";}
 }
