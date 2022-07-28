9a4f98071d8f9d49bfc2ce0acebedc09d2a91336
niltonguiilhem <nil.guilhem@gmail.com>
2022-07-28 20:10:11 -0300

    Controller Methods Update

--- src/main/java/com/ngtechnology/estacionamento/controller/EstacionamentoController.java
+++ src/main/java/com/ngtechnology/estacionamento/controller/EstacionamentoController.java
@@ -17,27 +17,26 @@ public class EstacionamentoController {
     private VagaService service;
     /**Estudar conceito de request e response, te enviei um resumo do stackoverflow **/
 
-    /** modificar a nomenclatura do método, talvez para getAllVagas **/
+
     @GetMapping()
-    public Iterable<Vagas> get() {
+    public Iterable<Vagas> getAllVagas() {
         return service.findEstacionamento();}
 
-    /** modificar a nomenclatura do método, talvez para getIdVagas **/
+
     @GetMapping("/{id}")
-    public Optional<Vagas> get(@PathVariable("id") Long idvaga) {
+    public Optional<Vagas> getIdVagas(@PathVariable("id") Long idvaga) {
         return service.getVagaById(idvaga);
     }
 
-    /** modificar a nomenclatura do método, talvez para postVagas, lembrando que as nomenclaturas save, delete, find, são mais recorrentes
-     * nas services que interagem com o banco de dados **/
+
     @PostMapping
-    public ResponseEntity<Vagas> save(@RequestBody Vagas vagas){
+    public ResponseEntity<Vagas> postVagas(@RequestBody Vagas vagas){
         return new ResponseEntity<>(service.save(vagas),HttpStatus.CREATED);
     }
 
-    /** modificar a nomenclatura do método, talvez para putVagas **/
+
     @PutMapping("/{id}")
-    public ResponseEntity<Vagas> put (@PathVariable ("id") Long id, @RequestBody Vagas vagas){
+    public ResponseEntity<Vagas> putVagas (@PathVariable ("id") Long id, @RequestBody Vagas vagas){
         Vagas v = service.update(vagas,id);
         return new ResponseEntity<>(v, HttpStatus.ALREADY_REPORTED);
     }
