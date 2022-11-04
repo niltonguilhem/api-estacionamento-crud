3dbbbc66758f99c3b6d18624c448c92161bb256e
niltonguiilhem <nil.guilhem@gmail.com>
2022-11-04 14:23:07 -0300

    @ControllerAdvice and Manuel Builder

--- src/main/java/com/ngtechnology/estacionamento/handler/exceptionHandler/Problema.java
+++ src/main/java/com/ngtechnology/estacionamento/handler/exceptionHandler/Problema.java
@@ -1,14 +1,43 @@
 package com.ngtechnology.estacionamento.handler.exceptionHandler;
 
-import lombok.Builder;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Builder
 public class Problema {
 
     private LocalDateTime dataHora;
     private String mensagem;
+
+    public Problema(){
+
+    }
+
+    public Problema(LocalDateTime dataHora, String mensagem){
+        this.dataHora = dataHora;
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+    public void setDataHora(LocalDateTime dataHora){
+        this.dataHora = dataHora;
+    }
+
+    public Problema withBuilderDataHora(LocalDateTime dataHora){
+        setDataHora(dataHora);
+        return this;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public Problema withBuilderMensagem(String mensagem){
+        setMensagem(mensagem);
+        return this;
+    }
 }
