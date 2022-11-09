71bbe43a0147e5ce847861ca363cdddb9f0c1389
niltonguiilhem <nil.guilhem@gmail.com>
2022-11-09 09:19:12 -0300

    handleExceptionInternal

--- src/main/java/com/ngtechnology/estacionamento/handler/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/ngtechnology/estacionamento/handler/exceptionHandler/ApiExceptionHandler.java
@@ -2,44 +2,47 @@ package com.ngtechnology.estacionamento.handler.exceptionHandler;
 
 import com.ngtechnology.estacionamento.handler.exception.EntidadeInexistenteException;
 import com.ngtechnology.estacionamento.handler.exception.PartnerException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntidadeInexistenteException.class)
-    public ResponseEntity<Problema> handlerIdNaoEncontradoException(
-            EntidadeInexistenteException exception) {
-        Problema problema = new Problema()
-                .withBuilderDataHora(LocalDateTime.now())
-                .withBuilderMensagem(exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problema);
+    public ResponseEntity<?> handlerIdNaoEncontradoException(
+            EntidadeInexistenteException exception, WebRequest request) {
+        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(PartnerException.class)
-    public ResponseEntity<Problema> handlerPartnerNaoEncontradoException(
-            PartnerException exceptionPartner) {
-        Problema problema = new Problema()
-                .withBuilderDataHora(LocalDateTime.now())
-                .withBuilderMensagem(exceptionPartner.getMessage());
-        return  ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problema);
+    public ResponseEntity<?> handlerPartnerNaoEncontradoException(
+            PartnerException exceptionPartner, WebRequest request) {
+        return handleExceptionInternal(exceptionPartner, exceptionPartner.getMessage(), new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
+
     }
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<Problema> handlerHttpMediaTypeNotSupportedException(){
-        Problema problema = new Problema()
-                .withBuilderDataHora(LocalDateTime.now())
-                .withBuilderMensagem("O tipo de mídia não é aceito.");
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                .body(problema);
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        if (body == null) {
+            body = new Problema()
+                    .withBuilderDataHora(LocalDateTime.now())
+                    .withBuilderMensagem(status.getReasonPhrase());
+        } else if (body instanceof  String) {
+            body = new Problema()
+                    .withBuilderDataHora(LocalDateTime.now())
+                    .withBuilderMensagem((String) body);
+        }
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
 
--- src/main/java/com/ngtechnology/estacionamento/service/VagasService.java
+++ src/main/java/com/ngtechnology/estacionamento/service/VagasService.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
 
 
 import java.util.List;
